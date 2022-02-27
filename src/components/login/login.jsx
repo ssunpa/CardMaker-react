35ebb0509694897d4049d36453b0367c12ae1249
ssunpa <bestpa.1130@gmail.com>
2022-02-27 21:53:10 +0900

    add login and logout funtionality

--- src/components/login/login.jsx
+++ src/components/login/login.jsx
@@ -1,35 +1,47 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
 import Footer from '../footer/footer';
 import Header from '../header/header';
 import styles from './login.module.css';
 
 const Login = ({ authService }) => {
-  const onLogin = event => {
-    authService //
-      .login(event.currentTarget.textContent)
-      .then(console.log);
-  };
-  return (
-    <section className={styles.login}>
-      <Header />
-      <section>
-        <h1>Login</h1>
-        <ul className={styles.list}>
-          <li className={styles.item}>
-            <button className={styles.button} onClick={onLogin}>
-              Google
-            </button>
-          </li>
-          <li className={styles.item}>
-            <button className={styles.button} onClick={onLogin}>
-              Github
-            </button>
-          </li>
-        </ul>
-      </section>
-      <Footer />
-    </section>
-  );
+    const navigate = useNavigate();
+    const goToMaker = (userId) => {
+        navigate('/maker', { state: { id: userId } });
+    };
+    const onLogin = (event) => {
+        authService //
+            .login(event.currentTarget.textContent)
+            .then((data) => goToMaker(data.user.uid));
+    };
+
+    useEffect(() => {
+        authService.onAuthChange((user) => {
+            user && goToMaker(user.uid);
+        });
+    });
+
+    return (
+        <section className={styles.login}>
+            <Header />
+            <section>
+                <h1>Login</h1>
+                <ul className={styles.list}>
+                    <li className={styles.item}>
+                        <button className={styles.button} onClick={onLogin}>
+                            Google
+                        </button>
+                    </li>
+                    <li className={styles.item}>
+                        <button className={styles.button} onClick={onLogin}>
+                            Github
+                        </button>
+                    </li>
+                </ul>
+            </section>
+            <Footer />
+        </section>
+    );
 };
 
 export default Login;
